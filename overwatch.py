d8a2bac8e74545e406810d25d374822860ed34bf
Xuerian <sky.shell@gmail.com>
2013-04-08 02:51:12 -0400

    Rewrite around modular structure, add channel/nick completion

--- overwatch.py
+++ overwatch.py
@@ -1,37 +1,217 @@
 __module_name__ = "overwatch-mode"
-__module_version__ = "0.2"
+__module_version__ = "0.3"
 __module_description__ = "Provides meta-tabs which can watch and interact with multiple channels"
 
-__server_name__ = ">Overwatch"
-__focus_on_start__ = True
+__server_name__ = "[Overwatch]"
+__focus_on_load__ = True
 
 import xchat
 from time import time
 import re
 from collections import deque
 
+SHIFT = 1
+CTRL = 4
+ALT = 8
 
-def error(msg):
-    our_context.prnt("\012Overwatch Error:\017 " + msg)
+TAB = 0xFF09
+LEFT_TAB = 0xFE20
+ENTER = 0xFF0D
 
+events_decoded = {}  # Event strings decoded with channel added
+events_inline = {}  # Decoded strings with channel hidden
 
-def get_input():
-    return our_context.get_info("inputbox")
-
-
-def set_input(line, move_cursor=True):
-    our_context.command("settext " + line)
-    if move_cursor:
-        set_input_pos(len(line))
-
-
-def set_input_pos(pos):
-    our_context.command("setcursor " + str(pos))
+overwatches = {}  # Shouldn't double up
+greedy_overwatch = None
+channel_map = {}
+padding = ["", "", ""]  # Prevents format from complaining on short events
 
 
-recent_events = deque([], 25)
-recent_users = {}
-recent_channels = {}
+class xbuffer:
+    initial_load = False
+
+    def __init__(self, name, is_server=True):
+        self.name = name
+        self.context = xchat.find_context(channel=name)
+        if not self.context:
+            self.initial_load = True
+            if is_server:
+                xchat.command("newserver -noconnect "+name)
+            else:
+                xchat.command("query "+name)
+            self.context = xchat.find_context(channel=name)
+
+    def focus(self):
+        self.context.set()
+        self.context.command("gui focus")
+
+    def get_input(self):
+        return self.context.get_info("inputbox")
+
+    def get_input_cursor(self):
+        return self.context.get_prefs("state_cursor")
+
+    def set_input(self, value, move_cursor=True):
+        self.context.command("settext " + value)
+        if move_cursor:
+            self.set_input_cursor(len(value))
+
+    def set_input_cursor(self, pos):
+        self.context.command("setcursor " + str(pos))
+
+
+class overwatch:
+    focus_on_load = __focus_on_load__  # Opt
+    is_greedy = True  # Opt
+    greedy_blacklist = []  # Opt
+    last_channel = ""
+    last_action = time()
+    recent_channels = {}
+    recent_users = {}
+    channels = []
+    auto_type = 0
+    auto_list = deque()
+    auto_first = True
+
+    def __init__(self, name=__server_name__):
+        global overwatches, greedy_overwatch
+        self.buffer = xbuffer(name)
+        if self.buffer.initial_load and self.focus_on_load:
+            self.buffer.focus()
+        overwatches[name] = self
+        if self.is_greedy:
+            greedy_overwatch = self
+
+    def echo(self, *args):
+        self.buffer.context.prnt(', '.join(str(i) for i in args))
+
+    def err(self, *args):
+        self.echo("Overwatch %s error" % self.buffer.name, *args)
+
+    def auto_list_channels(self, search=""):
+        self.auto_type = 1
+        recent = sorted([k for k in self.recent_channels if k.startswith(search)], key=lambda k: self.recent_channels[k])
+        full = sorted([k for k in self.channels if k not in recent and k.startswith(search)])
+        self.auto_list.extend(recent + full)
+
+    def auto_list_users(self, channel, search=""):
+        self.auto_type = 2
+        p = re.compile(search, re.I)
+        recent = sorted([k for k, v in self.recent_users.items() if bool(p.match(k)) and v[0] == channel], key=lambda k: self.recent_users[k][1])
+        channel_context = xchat.find_context(channel=channel)
+        if channel_context:
+            channel_context.set()
+            full = sorted([x.nick for x in channel_context.get_list("users") if bool(p.match(x.nick)) and x.nick not in recent])
+            print full, [x.nick for x in channel_context.get_list("users")]
+            self.auto_list.extend(recent + full)
+        else:
+            self.auto_list.extend(recent)
+
+    def pressed_tab(self, modifiers):
+        words = self.buffer.get_input().strip().split(" ", 2)
+        num = len(words)
+        if self.auto_first:
+            self.auto_first = False
+            if num == 1:
+                # Tab to next channel
+                if words[0] in self.channels:
+                    self.auto_list_channels()
+                # Complete channel name
+                elif words[0] and not words[0].startswith("#"):
+                    self.auto_list_channels("#" + words[0])
+                # Search partial chanel
+                else:
+                    self.auto_list_channels(words[0])
+            # Search nicks
+            else:
+                if words[1]:
+                    self.auto_list_users(words[0], words[1])
+
+        if self.auto_list:
+            # Complete channel
+            if self.auto_type == 1:
+                rest = len(words) > 1 and words[1:].join(" ") or ""
+                self.buffer.set_input(self.auto_list[0] + " " + rest)
+            # Complete nick
+            else:
+                rest = self.buffer.set_input("%s %s%s %s" % (words[0], self.auto_list[0], xchat.get_prefs("completion_suffix"), len(words) > 2 and words[2:].join(" ") or ""))
+            self.auto_list.rotate(1)
+
+        return xchat.EAT_ALL
+
+    def pressed_enter(self, modifiers):
+        if self.last_channel and not self.buffer.get_input():
+            self.buffer.set_input(self.last_channel + " ")
+            return xchat.EAT_ALL
+        return xchat.EAT_NONE
+
+    def pressed_any(self, key, modifiers, word):
+        self.last_action = time()
+        if not self.auto_first:
+            self.auto_first = True
+            self.auto_list.clear()
+            self.auto_type = None
+        return xchat.EAT_NONE
+
+    def on_event(self, channel, event, word, word_eol):
+        # Add to buffer
+        which_list = (channel == self.last_channel and events_inline or events_decoded)
+        self.echo(which_list[event].format(channel, *(word + padding)))
+        # Update recents
+        self.recent_channels[channel] = now = time()
+        self.recent_users[word[0]] = (channel, time())
+        # Update prompt
+        if now - self.last_action > 5:
+            line = self.buffer.get_input()
+            if not line or line == self.last_channel + " ":
+                self.buffer.set_input(channel + " ")
+        self.last_channel = channel
+
+    def on_send(self, word, word_eol):
+        if not word or not word[0].startswith("#"):
+            if self.last_channel:
+                # Add missing channel prefix
+                line = self.last_channel + " " + word_eol[0]
+                self.buffer.set_input(line)
+        elif word:
+            context = xchat.find_context(channel=word[0])
+            # Send message and restore channel prefix
+            if context:
+                context.command("msg " + word_eol[0])
+                self.buffer.set_input(word[0] + " ")
+            else:
+                # No valid target channel
+                self.err("Target channel %s not found" % word[0])
+        return xchat.EAT_ALL
+
+    def watch_channel(self, channel):
+        global channel_map
+        if channel.startswith("#"):
+            if not channel in self.channels:
+                self.channels.append(channel)
+                channel_map[channel] = self
+
+    def is_focused(self):
+        return xchat.get_info("channel") == self.name
+
+
+class recent_list:
+    def __init__(self):
+        self.dict = {}
+
+    def add(self, k):
+        self.dict[k] = time()
+
+    def has(self, key):
+        return key in self.dict.keys()
+
+    def list(self, search=""):
+        if not self.dict:
+            return []
+        if search:
+            return sorted([key for key in self.dict if key.startswith(search)], key=lambda k: self.dict[k])
+        return sorted(self.dict.keys(), key=lambda k: self.dict[k])
+
 
 # xchat to python escape map
 escapes = {
@@ -58,8 +238,6 @@ def decode(text):
     return text
 
 # Find desired event strings
-events_decoded = {}  # Event strings decoded with channel added
-events_inline = {}  # Decoded strings with channel hidden
 chat_events = [
     "Channel Message",
     "Channel Msg Hilight",
@@ -72,55 +250,35 @@ chat_events = [
 ]
 
 
-# Decode strings from Text Event settings
-re_move = re.compile(r"(.+)(%C\d*)(\$t)(.*)")  # Message color code needs to be put after tab char
-next = ""
-with open("config/pevents.conf") as f:
-    for line in f:
-        words = line.split("=", 1)
-        if words and words[0] != "\n":
-            (key, value) = words
-            value = value.strip()
-            if key == "event_name" and value in chat_events:
-                next = value
-            elif key == "event_text" and next:
-                decoded = decode(re_move.sub(r"\1\3\2\4", value))
-                events_decoded[next] = decoded.replace("\t", "\t\0036\010(\010{0}\010)\010\017 ")
-                events_inline[next] = decoded.replace("\t", "\t\010({0})\010 ")
-                next = ""
-
-# Acquire our context
-our_context = xchat.find_context(channel=__server_name__)
-if not our_context:
-    xchat.command("newserver -noconnect "+__server_name__)
-    our_context = xchat.find_context(channel=__server_name__)
-
-
-def is_focused():
-    return xchat.get_info("channel") == __server_name__
+def compile_strings():
+    global events_decoded, events_inline
+    # Decode strings from Text Event settings
+    re_move = re.compile(r"(.+)(%C\d*)(\$t)(.*)")  # Message color code needs to be put after tab char
+    next = ""
+    with open(xchat.get_info("xchatdir") + "/pevents.conf") as f:
+        for line in f:
+            words = line.split("=", 1)
+            if words and words[0] != "\n":
+                (key, value) = words
+                value = value.strip()
+                if key == "event_name" and value in chat_events:
+                    next = value
+                elif key == "event_text" and next:
+                    decoded = decode(re_move.sub(r"\1\3\2\4", value))
+                    events_decoded[next] = decoded.replace("\t", "\t\0036\010(\010{0}\010)\010\017 ")
+                    events_inline[next] = decoded.replace("\t", "\t\010({0})\010 ")
+                    next = ""
 
 
 # Set up chat callbacks
-last_channel = ""
-last_action = time()
-padding = ["", "", ""]  # Prevents format from complaining
-
-
 def chat_callback(event, word, word_eol):
     global last_channel
-    if not is_focused():
-        channel = xchat.get_info("channel")
-       # Add event to overwatch
-        which_list = channel == last_channel and events_inline or events_decoded
-        our_context.prnt(which_list[event].format(channel, *(word + padding)))
-        # Update data
-        now = recent_channels[channel] = recent_users[word[0]] = time()
-        # Update prompt
-        if now - last_action > 5:
-            line = get_input()
-            if not line or line == last_channel + " ":
-                set_input(channel + " ")
-        last_channel = channel
+    channel = xchat.get_info("channel")
+    if channel in channel_map and channel_map[channel]:
+        return channel_map[channel].on_event(channel, event, word, word_eol)
+    elif greedy_overwatch:
+        greedy_overwatch.watch_channel(channel)
+        return channel_map[channel].on_event(channel, event, word, word_eol)
     return xchat.EAT_NONE
 
 
@@ -130,78 +288,47 @@ def clone_chat_event(event):
     xchat.hook_print(event, clone)
 
 
-map(clone_chat_event, chat_events)
-
-
-# Handle key presses
-SHIFT = 1
-CTRL = 4
-ALT = 8
-
-TAB = 0xFF09
-LEFT_TAB = 0xFE20
-ENTER = 0xFF0D
-
-
-def on_tab(key, modifier):
-    print "tab", key, modifier
-    return xchat.EAT_ALL
-
-
-# def on_enter(modifier):
-#     print recent_channels, recent_users
-#     line = xchat.get_info("inputbox")
-#     if not line.startswith("/") or line.startswith("/em"):
-#         return xchat.EAT_ALL
-#     return xchat.EAT_NONE
-
-
 # Hook keys
 def key_press(word, word_eol, data):
-    global last_action
-    if is_focused():
+    focus = xchat.get_info("channel")
+    if focus in overwatches:
         key, modifiers = int(word[0]), int(word[1])
-        if key == TAB or key == LEFT_TAB:
-            if not modifiers & (CTRL | ALT | SHIFT):
-                return on_tab(key, modifiers)
-        elif key == ENTER:
-            if not xchat.get_info("inputbox") and last_channel:
-                set_input(last_channel + " ")
-                return xchat.EAT_ALL
-        # else:
-        #     print word
-        #     return on_enter(modifiers)
-        # word[2] contains keys with names
-        last_action = time()
-
+        if key == ENTER:
+            return overwatches[focus].pressed_enter(modifiers)
+        elif key == TAB or key == LEFT_TAB:
+            return overwatches[focus].pressed_tab(modifiers)
+        else:
+            return overwatches[focus].pressed_any(key, modifiers, word)
+# not modifiers & (CTRL | ALT | SHIFT)
     return xchat.EAT_NONE
 
 
 # Hook sending messages
 def on_send(word, word_eol, data):
-    if is_focused():
-        # No target channel
-        if not word or not word[0].startswith("#"):
-            line = last_channel + " " + word_eol[0]
-            set_input(line)
-            set_input_pos(len(line))
-        elif word:
-            # Found target channel
-            context = xchat.find_context(channel=word[0])
-            if context:
-                context.command("msg " + word_eol[0])
-                set_input(word[0] + " ")
-            # Invalid target channel
-            else:
-                error("Target channel %s not found" % word[0])
-                set_input(word_eol[0])
-        return xchat.EAT_ALL
+    focus = xchat.get_info("channel")
+    if focus in overwatches:
+        return overwatches[focus].on_send(word, word_eol)
     return xchat.EAT_NONE
 
-xchat.hook_print("Key Press", key_press)
-xchat.hook_command("", on_send)
 
-if __focus_on_start__:
-    our_context.command("gui focus")
+main = None
 
-print __module_name__, __module_version__, 'loaded'
+
+# Defer load until config files stabilize
+def load(*args):
+    global main
+    compile_strings()
+    map(clone_chat_event, chat_events)
+
+    main = overwatch(__server_name__)
+
+    xchat.hook_print("Key Press", key_press)
+    xchat.hook_command("", on_send)
+
+    if greedy_overwatch:
+        for x in xchat.get_list("channels"):
+            greedy_overwatch.watch_channel(x.channel)
+
+    main.echo(__module_name__, __module_version__, main.buffer.initial_load and 'loaded' or 'reloaded')
+
+xchat.hook_timer(100, load)
